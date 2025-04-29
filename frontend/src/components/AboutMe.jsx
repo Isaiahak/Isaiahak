65b57b6b4927cb967af7f81a3fa7c7f07488be10
isaiah <isaiahkenny321123@gmail.com>
2025-04-28 23:53:09 -0400

    updates

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -5,17 +5,57 @@ import {useState, useEffect} from "react"
 function AboutMe(){
 	
 	const [currentIndex, setCurrentIndex] = useState(0)
+	const [progress, setProgress] = useState(0)
 
 	const aboutMeSections = ["one section aobut me as a person ","section about what i am learning and want to learn"]
-
-	us
-
 
 	function setCurrent(){
 		setCurrentIndex( currentIndex >= aboutMeSections.length - 1? 0 : currentIndex + 1 )
 	}
+	/*
+	useEffect(()=>{
+		incrementBar()
+	})
+	*/
+	function incrementBar(){
+		if(progress === 10000){
+			setCurrent()
+		}
+		setProgress( progress === 10000 ? 0 : progress + 1)
 
-	setTimeout(setCurrent, 10000)
+	}
+
+	const ProgressBar = () =>{
+		var array = []
+		var element = <span></span>
+		for (let i  = 0; i < progress ;i = i +10){
+			if (i === 0){
+				(i) => {
+					 element = <span key={i} className="start-component"></span>
+				}
+
+			}
+			else if (i === progress - 1){
+				//creates the end rounded point
+				(i) => {
+					 element = <span key={i} className="end-component"></span>
+				}
+			}
+			else{
+				// creates middle points
+				(i) => {
+						 element = <span key={i} className="component"></span>
+				}
+			}
+			array[i] = element
+		}
+
+		return(
+			<div className="progress-bar">
+				{array}
+			</div>
+		)
+	}
 
 	const AboutMeDescriptions = () =>{
 		var text = aboutMeSections[currentIndex]
@@ -33,8 +73,9 @@ function AboutMe(){
 			<div className="description-container">
 				<AboutMeDescriptions/>
 			</div>
-		</div>
-		<LanguagesAndTools/>
+			<ProgressBar/>
+			<LanguagesAndTools/>
+		</div>	
 		</section>
 	)
 }
