d3e028aa18b4ec68d08ddb89be964e98955519e3
Isaiah <isaiahkenny321123@gmail.com>
2025-04-30 03:40:24 -0400

    the tool icons

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -1,81 +1,77 @@
 import "../styling/AboutMe.css"
 import LanguagesAndTools from "./LanguagesAndTools.jsx"
-import {useState, useEffect} from "react"
+import {useState, useEffect, useRef} from "react"
 
 function AboutMe(){
 	
 	const [currentIndex, setCurrentIndex] = useState(0)
-	const [progress, setProgress] = useState(0)
-
-	const aboutMeSections = ["one section aobut me as a person ","section about what i am learning and want to learn"]
-
+	const [count, setCount] = useState(0)
+	const startTime = useRef(new Date().getMilliseconds())
+	const time = useRef(new Date().getMilliseconds())
+	const aboutMeSections = [
+		" Hello my name is Isaiah Ashton-Kenny, I am a software engineering student, with a love for all things"
+		+"software, music and outdoors. When I am not throwing myself at some new idea or struggling to learn something"
+		+"embarrassingly simple. You can find me outside running, riding my bike or climbing rocks",
+		" Currently I am sharpening my skills in full-stake development by working on [add a project with enzo]\n" 
+		+" with the focus being on becoming more familiar the dev-ops and the development lifecycle \n"
+		+" and  a game using the programming language Odin to improve performance and data oriented design \n"
+	]
 	function setCurrent(){
-		setCurrentIndex( currentIndex >= aboutMeSections.length - 1? 0 : currentIndex + 1 )
+		setCurrentIndex(prevIndex => prevIndex === aboutMeSections.length -1 ? 0 : prevIndex + 1)
 	}
-	/*
-	useEffect(()=>{
-		incrementBar()
-	})
-	*/
-	function incrementBar(){
-		if(progress === 10000){
-			setCurrent()
-		}
-		setProgress( progress === 10000 ? 0 : progress + 1)
+	
+	function incrementCount(){
+		setCount(prevCount => prevCount === 400 ? 0 : prevCount + 1)
+	}
 
-	}
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			time.current = new Date().getMilliseconds()
+			if (time.current < 25){
+				startTime.current = new Date().getMilliseconds()
+			}
+			var diff = time.current - startTime.current
+			if(diff >= 25){
+				incrementCount()
+				startTime.current = time.current
+			}
+		}, 1)
+		return () => clearInterval(intervalId)
+	},[count]);
+
+	useEffect(() => {
+		if(count == 400){
+			setCurrent()
+			setCount(0)
+		}
+	},[count])
 
 	const ProgressBar = () =>{
-		var array = []
-		var element = <span></span>
-		for (let i  = 0; i < progress ;i = i +10){
-			if (i === 0){
-				(i) => {
-					 element = <span key={i} className="start-component"></span>
-				}
-
-			}
-			else if (i === progress - 1){
-				//creates the end rounded point
-				(i) => {
-					 element = <span key={i} className="end-component"></span>
-				}
-			}
-			else{
-				// creates middle points
-				(i) => {
-						 element = <span key={i} className="component"></span>
-				}
-			}
-			array[i] = element
-		}
-
-		return(
-			<div className="progress-bar">
-				{array}
-			</div>
-		)
+		return (
+	    <div className="progress-bar">
+	      {[...Array(count)].map((_, i) => (
+	        <span key={i} className={`item ${ i ===0 && i >= count - 1 ? "start-end-component" : i === 0 ? "start-component" : i === count - 1 ? "end-component": "component"}`}></span>
+	      ))}
+	    </div>
+	  );	
 	}
 
 	const AboutMeDescriptions = () =>{
 		var text = aboutMeSections[currentIndex]
 		return(
-			<div>
-				<p>{text}</p>
-				<div></div>
+			<div className="description-container">
+				<p className="description">{text}</p>
 			</div>
 		)
 	}
 	
 	return(
 		<section id="about">
-		<div className="aboutme-container">
-			<div className="description-container">
+			<div className="aboutme-container">				
 				<AboutMeDescriptions/>
-			</div>
-			<ProgressBar/>
-			<LanguagesAndTools/>
-		</div>	
+				<ProgressBar/>
+				<LanguagesAndTools/>
+			</div>	
 		</section>
 	)
 }
