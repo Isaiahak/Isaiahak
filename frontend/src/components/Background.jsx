f753fad183059ec90b568707753c6496e51cf584
isaiah <isaiahkenny321123@gmail.com>
2025-08-29 00:05:58 -0400

    fixing navbar

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -1,6 +1,6 @@
 export default function background(){
   return(
-  	<div className="fixed w-full h-full mx-auto -z-10 bg-red-100"
+  	<div className="fixed w-full h-full mx-auto -z-10 bg-primary"
   		/>
 	)
 }
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,12 +1,15 @@
 const Nav = () =>{
 
 	return(
-		<nav className="fixed flex w-full md:h-[4rem] md:h-min-12 border-b-primary border-b justify-center place-content-center z-100 ">
-			<div className=" absolute self-center w-full h-full backdrop-blur-lg opacity-30 bg-black z-10"/>
+		<nav className="fixed flex w-full md:h-[4rem] md:h-min-12 border-b-primary border-b  backdrop-blur-md justify-center place-content-center z-100 ">
+			<div className=" absolute self-center w-full h-full backdrop-blur-md opacity-30 z-10"/>
 			<div className="self-center w-full md:w-[90rem] md:h-[4rem] flex flex-row flex-wrap z-11">
-				<a href="#about" className="w-[33%]  p-4 mx-auto text-black hover:bg-secondary bg-primary text-center   ">ABOUT</a>
-				<a href="#projects" className="w-[33%]  p-4 mx-auto text-black hover:bg-secondary bg-primary text-center  ">PROJECTS</a>
-				<a href="#quicklinks" className="w-[33%] p-4 mx-auto text-black hover:bg-secondary bg-primary text-center ">LINKS</a>
+				<div className="flex flex-col mx-auto">
+					<a href="#about" className=" pt-4 mx-auto text-black text-center ">ABOUT</a>
+					<span className=" h-[1px] animate-hover-bar"/>
+				</div>
+				<a href="#projects" className="  p-4 mx-auto text-black text-center animate-hover-bar ">PROJECTS</a>
+				<a href="#quicklinks" className=" p-4 mx-auto text-black text-center animate-hover-bar ">LINKS</a>
 			</div>
 		</nav>
 	)
