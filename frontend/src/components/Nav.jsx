2b023f6beaca5a9e6cb15738ca80baeae8173498
Isaiah Ashton-Kenny <isaiahkenny321123@gmail.com>
2026-02-02 21:56:44 -0500

    fixed nav reverted changes

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,15 +1,12 @@
-const Nav = () =>{
+const Nav = () => {
 
-	return(
+	return (
 		<nav className="fixed flex w-full md:h-[4rem] md:h-min-12 border-b-primary border-b  backdrop-blur-md justify-center place-content-center z-100 ">
-			<div className=" absolute self-center w-full h-full backdrop-blur-md opacity-30 z-10"/>
-			<div className="self-center w-full md:w-[90rem] md:h-[4rem] flex flex-row flex-wrap z-11">
-				<div className="flex flex-col mx-auto">
-					<a href="#about" className=" pt-4 mx-auto text-black text-center ">ABOUT</a>
-					<span className=" h-[1px] animate-hover-bar"/>
-				</div>
-				<a href="#projects" className="  p-4 mx-auto text-black text-center animate-hover-bar ">PROJECTS</a>
-				<a href="#quicklinks" className=" p-4 mx-auto text-black text-center animate-hover-bar ">LINKS</a>
+			<div className=" absolute self-center w-full h-full backdrop-blur-md opacity-30 z-10" />
+			<div className="self-center w-full md:w-[90rem] md:h-[4rem] flex flex-row z-20">
+				<a href="#about" className="p-4 mx-auto text-black text-center">ABOUT</a>
+				<a href="#projects" className="p-4 mx-auto text-black text-center">PROJECTS</a>
+				<a href="#quicklinks" className="p-4 mx-auto text-black text-center">LINKS</a>
 			</div>
 		</nav>
 	)
